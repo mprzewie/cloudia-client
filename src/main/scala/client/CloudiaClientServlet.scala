860bb33ef8ebcbd3e745c5c2667dd6b6f40462bf
Marcin Przewięźlikowski <m.przewie@gmail.com>
2017-06-13 23:32:55 +0200

    Started working on frontend.

--- src/main/scala/client/CloudiaClientServlet.scala
+++ src/main/scala/client/CloudiaClientServlet.scala
@@ -11,6 +11,7 @@ import akka.pattern.ask
 import cloudia.Jason
 
 import scala.concurrent.ExecutionContext.Implicits.global
+import scala.util.parsing.json.JSONObject
 
 
 
@@ -19,21 +20,35 @@ class CloudiaClientServlet extends CloudiaclientStack {
 
   implicit val timeout = akka.util.Timeout(new FiniteDuration(1, SECONDS)) // Timeout for the resolveOne call
   val system = ActorSystem("cloudia-client")
+  val server = system.actorSelection("akka.tcp://cloudia-server@127.0.0.1:8888/user/server")
+
 
   get("/") {
-    <html>
-      <body>
-        <h1>Hello, world!</h1>
-        Say
-        <a href="hello-scalate">hello to Scalate</a>
-        .
-      </body>
-    </html>
+
+    contentType="text/html"
+
+//      val index = Await.result(server.ask(Handshake())(1 second).mapTo[DirectoryIndex], 1 second)
+    try {
+
+      val futureIndex = server.ask(Handshake())(1 second).mapTo[DirectoryIndex]
+      val index = Await.result(server.ask(Handshake())(1 second).mapTo[DirectoryIndex], 1 second)
+      jade("twonodes",
+        "thisNode" -> "this",
+        "thatNode" -> "that",
+        "thisContents" -> Jason.toJson(index),
+        "thatContents" -> JSONObject(Map()))
+    }
+    catch {
+      case _:Exception =>
+        jade("error")
+    }
+
+
+//    jade("hello-scalate", "headline" -> "korwo")
   }
 
   get("/cloudia") {
 
-    val server = system.actorSelection("akka.tcp://cloudia-server@127.0.0.1:8888/user/server")
     server.resolveOne().onComplete {
       case actor =>
         println(actor)
@@ -42,7 +57,7 @@ class CloudiaClientServlet extends CloudiaclientStack {
 
     val index = Await.result(server.ask(Handshake())(1 second).mapTo[DirectoryIndex], 1 second)
 
-    contentType = "text/html"
+    contentType = "application/json"
 
     Jason.toJson(index)
 
